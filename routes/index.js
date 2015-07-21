c73bc90b3b399e2cd1a10d0e28483b339969416f
QCmu <mu.qiaochu@gmail.com>
2015-07-21 16:28:14 -0400

    Add Reg Page (Get)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HOME' });
 });
-
+router.get('/reg',function(req,res){
+	res.render('reg',{
+		title:'Registration'
+	});
+});
 module.exports = router;
